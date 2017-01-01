b3605a8378f6e2e038c385712c0b25a139d3828b
Deana Fajen <dlfajen@hotmail.com>
2017-01-01 16:26:18 -0600

    Added ability to read files in a directory specified by the user
    Added ability to read files in a directory specified by the user

--- inventory.py
+++ inventory.py
@@ -11,12 +11,20 @@ COUNT = 3
 inv_dict = dict()
 i = 0
 
-file_list = glob.glob('*-Inventory.txt')
+dir_name = raw_input("Enter the directory where the inventory files are (ex: C:\\temp or leave blank if the files are in the current directory): ")
+if not dir_name:
+    dir_name = '.\\'
+    
+if not dir_name.endswith('\\'):
+    dir_name = dir_name + '\\'
+
+file_list = glob.glob(dir_name + '*-Inventory.txt')
 
 for file in file_list:
     inv_file = list(csv.reader(open(file, 'rb'), delimiter='\t'))
-    file_name_list = file.split("-Inventory.txt")
-    char_name = file_name_list[0];
+    file_name_list = file.split(dir_name)
+    char_name_list = file_name_list[1].split("-Inventory.txt")
+    char_name = char_name_list[0];
 
     for x in range(1, len(inv_file)):
         key = i
